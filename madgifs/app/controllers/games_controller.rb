415134f437321475c3a4b7cc544c25e1bd00c20c
jemise111 <jesse.sessler@gmail.com>
2014-04-03 15:41:20 -0400

    partial commit

--- madgifs/app/controllers/games_controller.rb
+++ madgifs/app/controllers/games_controller.rb
@@ -1,73 +1,38 @@
-class UsersController < ApplicationController
-  def index
-    @users = User.all
-  end
+class GamesController  < ApplicationController
 
-  def show
-    @user = User.find(params[:id])
-  end
+def index
+  @user = User.find(params[:user_id])
+  @user.games
+end
 
-  def new
-    # only admins can create users while logged in
-    require_admin_authentication if current_user
-    @user = User.new
-  end
+def show
+  @game = Game.find(params[:id])
+end
 
-  def create
-    # only admins can create users while logged in
-    require_admin_authentication if current_user
-    @user = User.new(user_params)
-    @user.update(admin: false, score: 0)
-    if @user.save
-      # user is logged in upon creation
-      session[:user_id] = @user.id
-      redirect_to root_path
-    else
-      render 'new'
-    end
-  end
+def new
+  @user = User.find(params[:user_id])
+  @game = Game.new
+end
 
-  def edit
-    @user = User.find(params[:id])
-    # Users can only edit themselves unless they are admins
-    if current_user != @user
-      redirect_to root_path unless current_admin
-    end
-  end
+def create
+  @user = User.find(params[:user_id])
+  game = Game.create(games_params)
+  game.update(creator: current_user, player: @user)
+  redirect_to root_path
+end
 
-  def update
-    @user = User.find(params[:id])
-    # Users can only edit themselves unless they are admins
-    if current_user != @user && !current_admin
-      redirect_to root_path
-    else
-      if @user.update(user_params)
-        redirect_to(@user)
-      else
-        render 'edit'
-      end
-    end
-  end
+def update
+  @game = Game.find(params[:id])
+  @game.update(games_params)
+  redirect_to root_path
+end
 
-  def destroy
-    @user = User.find(params[:id])
-    # Users can only delete themselves unless they are admins
-    if current_user != @user && !current_admin
-      redirect_to root_path
-    else
-      @user.destroy
-      # Must be logged out if delete
-      session[:user_id] = nil unless current_admin
-      redirect_to(users_path)
-    end
-  end
+private
 
-  private
+def games_params
+  params.require(:user).permit(:sentence, :gif1, :gif2, :gif3, :gif4, :player)
+end
 
-  def user_params
-    params.require(:user).permit(:name,
-                                 :email,
-                                 :password,
-                                 :password_confirmation)
-  end
+
+
 end
--- madgifs/app/models/game.rb
+++ madgifs/app/models/game.rb
@@ -1,4 +1,26 @@
 class Game < ActiveRecord::Base
   belongs_to :creator, class_name: "User"
   belongs_to :player, class_name: "User"
+
+  def score
+
+    gif_score1= HTTParty.get("http://api.giphy.com/v1/gifs/search?q=burn&api_key=dc6zaTOxFJmzC")["data"].sample["url"] + ".gif"
+
+    gif_score2= HTTParty.get("http://api.giphy.com/v1/gifs/search?q=facepalm&api_key=dc6zaTOxFJmzC")["data"].sample["url"] + ".gif"
+
+    gif_score3= HTTParty.get("http://api.giphy.com/v1/gifs/search?q=whatever&api_key=dc6zaTOxFJmzC")["data"].sample["url"] + ".gif"
+
+    gif_score4 = HTTParty.get("http://api.giphy.com/v1/gifs/search?q=thumbs-up&api_key=dc6zaTOxFJmzC")["data"].sample["url"] + ".gif"
+
+    gif_score5=  HTTParty.get("http://api.giphy.com/v1/gifs/search?q=high-five&api_key=dc6zaTOxFJmzC")["data"].sample["url"] + ".gif"
+
+    gif_score6 =  HTTParty.get("http://api.giphy.com/v1/gifs/search?q=mic-drop&api_key=dc6zaTOxFJmzC")["data"].sample["url"] + ".gif"
+
+    music_score1 = HTTParty.get("http://developer.echonest.com/api/v4/song/search?api_key=CNQ7EJLGCHNW8QOIT&format=json&max_energy=.5&max_tempo=90")["response"]["songs"].sample["audio_summary"]["analysis_url"]
+
+    music_score2 = HTTParty.get("http://developer.echonest.com/api/v4/song/search?api_key=CNQ7EJLGCHNW8QOIT&format=json&min_energy=.5&max_energy=.8&min_tempo=90&max_tempo=120")["response"]["songs"].sample["audio_summary"]["analysis_url"]
+
+    music_score3 = HTTParty.get("http://developer.echonest.com/api/v4/song/search?api_key=CNQ7EJLGCHNW8QOIT&format=json&min_energy=.9&min_tempo=120&bucket=id:7digital-US&bucket=audio_summary&bucket=tracks")["response"]["songs"].sample["audio_summary"]["analysis_url"]
+
+  end
 end
