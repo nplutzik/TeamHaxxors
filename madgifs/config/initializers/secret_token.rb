ca5a0f2e35787d2ccadfa1fb966171107a8bb4ea
nplutzik <nplutzik@gmail.com>
2014-04-03 12:52:14 -0400

    ruby project created

---
+++ madgifs/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Madgifs::Application.config.secret_key_base = 'e169ba9ffb8c546077512d913d47772672a3ec79594589194e0d97e7980678f4331fc0b8037d58fc19491444f0c961da808866b6c9275bd881e887ea9a56fa8b'
---
+++ madgifs/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Madgifs::Application.config.session_store :cookie_store, key: '_madgifs_session'
